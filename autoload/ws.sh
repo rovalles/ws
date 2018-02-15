abdb45eea213f3938a6452cf6736853a81546137
Rodolfo Ovalles <rovalles0100@gmail.com>
2018-02-14 23:39:19 -0600

    version 2 of workspace. allows multiple directories.

--- autoload/ws.sh
+++ autoload/ws.sh
@@ -1,47 +1,91 @@
 #!/bin/bash
 
-FOLDER=""
-
-function changeDirectory(){
-    local newFolder="$1"
-
-    if [[ -d "$newFolder" && -n "$newFolder" && "$newFolder" != "$WS_DIR" ]] ; then
-        cd "$newFolder"
-    else
-        cd "$WS_DIR"
-    fi
+_WS_FOLDER=""
+_WS_CURRENT_INDEX=0
+_WS_PROJECT_LIST=(`echo ${WS_DIR}`)
+_WS_CURRENT_DIR=${_WS_PROJECT_LIST[$_WS_CURRENT_INDEX]}
+function _ws_change_directory(){
+    cd "$1"
+    _WS_FOLDER=""
 }
 
-function listFolders(){
+function _ws_display(){
+    local dumb="- - - - - W o r k s p a c e s - - - - -"
+    t=$1
+
+    dumb=(`echo ${dumb}`)
+    for d in "${dumb[@]}"
+    do
+        sleep $t
+        echo -n  "$d"
+    done
+
+    echo ""
+    echo ""
+}
+
+function _ws_help(){
+    local count=0
+    local _g="\e[32m"
+    local _w="\e[97m"
+
+    _ws_display 0
+
+
+    for each in "${_WS_PROJECT_LIST[@]}"
+    do
+        echo  "[$count] $each"
+        count=$(($count+1))
+    done
+    echo ""
+}
+
+function _ws_search(){
     local list
 
-    list=$(ls "$WS_DIR")
-    COMPREPLY=()
-
+    list=$(ls $_WS_CURRENT_DIR)
+    list+=" -h --help "
     COMPREPLY=( $( compgen -W "$list" -- "$1" ) )
-    FOLDER="$WS_DIR$COMPREPLY"
+
+    _WS_FOLDER="$_WS_CURRENT_DIR$COMPREPLY"
 }
 
-function _complete(){
+function _ws_complete(){
+    _WS_PROJECT_LIST=(`echo ${WS_DIR}`)
     local CUR=${COMP_WORDS[COMP_CWORD]}
     local PREV=${COMP_WORDS[COMP_CWORD-1]}
 
+    case "$PREV" in
+        [0-9])
+            _WS_CURRENT_INDEX="$PREV"
+            _WS_CURRENT_DIR=${_WS_PROJECT_LIST[$PREV]}
+            _ws_search "$CUR"
+        ;;
+        *)
+            _WS_CURRENT_DIR=${_WS_PROJECT_LIST[$_WS_CURRENT_INDEX]}
+            _ws_search "$CUR"
+        ;;
+    esac
+}
 
-    if [[ -n "$PREV" ]] ; then
-        case "$PREV" in
-            *)
-                listFolders "$CUR"
-            ;;
-        esac
+function _ws_submit(){
+    local submit="$1"
+
+    if [[ "$submit" == "-h" || "$submit" == "--help" ]] ; then
+        _ws_help
+    elif [ -n "$submit" ] ; then
+        _ws_change_directory "$_WS_FOLDER"
+    else
+        _ws_change_directory "$_WS_FOLDER"
     fi
 }
 
 function ws(){
-    changeDirectory "$FOLDER"
+    _ws_submit "$1"
 }
 
 if [[ -n "$WS_DIR" ]] ; then
-  complete -F _complete ws
+  complete -F _ws_complete ws
 else
-  echo "Set your absolute project path to WS_DIR"
+  echo "Set WS_DIR to your absolute project path in your ~/.bashrc"
 fi
